be37b7053a123ce8e81bd5693016713505ee96f2
opilane <opilane@HP-CZC2349HTM>
2023-01-09 17:06:15 +0200

    Add project files.

---
+++ 9.01/_9.01.py
@@ -0,0 +1,25 @@
+
+from func import *
+
+
+#print(date(2003,5,53))
+
+#print(is_prime(14))
+#print(bank(1000,2))
+#print(season(6))
+
+#print(square(16))
+
+
+##ans=arithmetic(2.5,1.8,"+")
+##print(ans)
+
+##ans=arithmetic(input(),input(),input())
+##print(ans)
+
+##if is_year_leap(input()):
+##    print("Liigaasta")
+##else:
+##    print("Tavaline aasta")
+
+
